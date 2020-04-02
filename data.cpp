74bbaf97ab3b4d4de861e8dce005358abc46495a
TauZer0 <gashamailov@edu.hse.ru>
2020-04-02 16:50:29 +0300

    Data: separate fields plot1_data, plot2_data and plot_fwi1_data instead of the vector.

--- data.cpp
+++ data.cpp
@@ -5,14 +5,22 @@ Data::Data() {
     double end = 5.0;
     double step = 0.001;
 
-    curves.emplace_back(begin, end, step, functions["sin"], "sin(x)", Qt::red);
-    curves.emplace_back(begin, end, step, functions["ln"], "ln(x)", Qt::blue);
-    curves.emplace_back(begin, end, step, functions["cos"], "cos(x)", Qt::green);
+    plot1_data = make_unique<FunctionData>(begin, end, step, functions["sin"], "sin(x)", Qt::red);
+    plot2_data = make_unique<FunctionData>(begin, end, step, functions["ln"], "ln(x)", Qt::blue);
+    plot_fwi1_data = make_unique<FunctionData>(begin, end, step, functions["cos"], "cos(x)", Qt::green);
 
-    curves[2].create_interval(0, 1);
-    curves[2].create_interval(3, 4);
+    plot_fwi1_data->create_interval(0, 1);
+    plot_fwi1_data->create_interval(0.5, 4);
 }
 
-const FunctionData& Data::get_curve(int i) const {
-    return curves[i];
+const FunctionData& Data::get_plot1_data() const {
+    return *plot1_data;
+}
+
+const FunctionData& Data::get_plot2_data() const {
+    return *plot2_data;
+}
+
+const FunctionData& Data::get_plot_fwi1_data() const {
+    return *plot_fwi1_data;
 }
--- data.h
+++ data.h
@@ -1,19 +1,26 @@
 #ifndef DATA_H
 #define DATA_H
 
+#include <memory>
 #include <vector>
 
 #include "functiondata.h"
 #include "functiontable.h"
 
+using std::make_unique;
 using std::vector;
+using std::unique_ptr;
 
 class Data {
  public:
     Data();
-    const FunctionData& get_curve(int i) const;
+    const FunctionData& get_plot1_data() const;
+    const FunctionData& get_plot2_data() const;
+    const FunctionData& get_plot_fwi1_data() const;
  private:
-    vector<FunctionData> curves;
+    unique_ptr<FunctionData> plot1_data;
+    unique_ptr<FunctionData> plot2_data;
+    unique_ptr<FunctionData> plot_fwi1_data;
     FunctionTable functions;
 };
 
